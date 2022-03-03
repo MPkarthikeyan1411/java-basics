7b1e5ca24b4e690f02c241b978723636078e91b1
MPkarthikeyan1411 <100142286+MPkarthikeyan1411@users.noreply.github.com>
2022-03-03 13:58:32 +0530

    basics1

---
+++ src/chartosring.java
@@ -0,0 +1,7 @@
+public class chartosring {
+    public static void main(String[] args){
+        char change='Q';
+        String string = String.valueOf(change);
+        System.out.println(""+string);
+    }
+}
