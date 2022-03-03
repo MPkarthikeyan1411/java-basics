d2adcecb8f13db90ee9161939db471205c383d9a
MPkarthikeyan1411 <100142286+MPkarthikeyan1411@users.noreply.github.com>
2022-03-03 14:11:23 +0530

    basics1

---
+++ src/chartoint.java
@@ -0,0 +1,9 @@
+public class chartoint {
+    public static void main(String[] args){
+        char C='9';
+        int a =Character.getNumericValue(C);
+        System.out.println(a);
+
+
+    }
+}
