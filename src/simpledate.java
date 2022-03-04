95c0fcedacb951da491d85008d58240741a69aec
MPkarthikeyan1411 <100142286+MPkarthikeyan1411@users.noreply.github.com>
2022-03-04 10:21:23 +0530

    basics1

---
+++ src/simpledate.java
@@ -0,0 +1,10 @@
+import java.sql.SQLOutput;
+import java.util.Date;
+public class simpledate {
+    public static void main(String[] arrgs)
+
+     {
+        Date date=new Date();
+    System.out.println(date);
+    }
+}
